2cc921fb68c3d36264b0ad6ec33907da3a0280a1
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2023-06-09 23:12:55 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'training-logger',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
