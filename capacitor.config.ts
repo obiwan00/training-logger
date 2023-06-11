f7028170a34de443f9f8c3318a918af54c1d8cbb
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2023-06-11 19:16:53 +0300

    add capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'training.logger',
   appName: 'training-logger',
   webDir: 'www',
   server: {
